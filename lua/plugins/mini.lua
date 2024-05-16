24f2e98df70dbfaef3b50b6e1af5f79ec6309713
makbari <mohammad-akbari@hotmail.com>
2024-05-16 21:45:09 +0200

    feat: add mini pairs for automatic paring

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -124,5 +124,22 @@ return {
       },
     },
   },
+  -- auto pairs
+  {
+    "echasnovski/mini.pairs",
+    opts = {
+      mappings = {
+        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
+      },
+    },
+    keys = {
+      {
+        "<leader>up",
+        function()
+          vim.g.minipairs_disable = not vim.g.minipairs_disable
+        end,
+        desc = "Toggle Auto Pairs",
+      },
+    },
+  },
 }
-
