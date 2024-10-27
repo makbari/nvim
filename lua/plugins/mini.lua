56ed91d9783e8da729adcc34a226163f4e1e4c25
Mohammad Akbari <mohammad-akbari@hotmail.com>
2024-10-27 09:34:52 +0100

    chore: remove duplicate declaration of the mini pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -107,23 +107,6 @@ return {
       end,
     },
   },
-  {
-    "echasnovski/mini.pairs",
-    opts = {
-      mappings = {
-        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
-      },
-    },
-    keys = {
-      {
-        "<leader>up",
-        function()
-          vim.g.minipairs_disable = not vim.g.minipairs_disable
-        end,
-        desc = "Toggle Auto Pairs",
-      },
-    },
-  },
   -- auto pairs
   {
     "echasnovski/mini.pairs",
